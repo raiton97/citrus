d7ca8d1aee00e8ff7dc056334a224d5c9dbba6ec
Inok <inok.dr189@gmail.com>
2021-11-13 11:32:04 +0700

    Initial Commit

---
+++ collect.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd /tmp/rom
+
+. build/envsetup.sh
+export CCACHE_DIR=/tmp/ccache
+export CCACHE_EXEC=$(which ccache)
+export USE_CCACHE=1
+ccache -M 20G
+ccache -o compression=true
+ccache -z
+lunch nad_juice-userdebug
+make nad -j8 &
+sleep 90m
+kill %1
+ccache -s
