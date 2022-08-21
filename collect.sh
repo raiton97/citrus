ad4b9ed8747f9e9f73106d210458cf3d33a02124
darknius20 <darknius20@gmail.com>
2022-08-21 16:26:14 +0000

    bring up aicp

--- collect.sh
+++ collect.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-cd /tmp/rom
+cd ~/rom
 
 . build/envsetup.sh
-export CCACHE_DIR=/tmp/ccache
+export CCACHE_DIR=~/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
-ccache -M 20G
-ccache -o compression=true
-ccache -z
+ccache -M 20G -F 0
+export BUILD_USERNAME="darknius"
+export BUILD_HOSTNAME="darx-labs"
 $lunch
+export ALLOW_MISSING_DEPENDENCIES=true
+export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
+export BUILD_BROKEN_DUP_RULES=true
 $make -j8 &
-sleep 105m
-kill %
+sleep 100m
+kill %1
 ccache -s
--- ziping.sh
+++ ziping.sh
@@ -2,7 +2,7 @@
 
 sleep 1m
 
-cd /tmp
+cd ~/
 
 com ()
 {
