9322039ae8f20bd4c0416327ba1269b93506eb8d
raiton97 raikiri <raitonraikiri97@gmail.com>
2023-05-26 18:03:08 +0000

    pb final

--- upload.sh
+++ upload.sh
@@ -1,5 +1,4 @@
-if [ -f ~/rom/out/target/product/juice/Spark-*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/juice/ && ls *.zip)"
-      rclone copy ~/rom/out/target/product/juice/Spark-*.zip fm293:juice -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/juice/ && ls *.zip) Uploaded Successfully!"
-fi
+curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/juice/ && ls *.zip)"
+rclone copy ~/rom/out/target/product/juice/PixelBlaster_3*.zip fm293:juice -P
+curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/juice/ && ls *.zip) Uploaded Successfully!"
+
