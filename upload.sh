17e5bd6e393037064b541313c195000a9303606f
raiton97 <133027621+raiton97@users.noreply.github.com>
2023-05-29 20:40:07 +0700

    nad jus final

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
 curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/juice/ && ls *.zip)"
-rclone copy ~/rom/out/target/product/juice/PixelBlaster_3*.zip fm293:juice -P
+rclone copy ~/rom/out/target/product/juice/Nusantara_v*.zip fm293:juice -P
 curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/juice/ && ls *.zip) Uploaded Successfully!"
 
