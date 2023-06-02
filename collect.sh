db2c4854b89b8808a614c9a6322ed3c8c7704b39
raiton97 <133027621+raiton97@users.noreply.github.com>
2023-06-02 20:06:11 +0700

    Rebrand to chime

--- collect.sh
+++ collect.sh
@@ -5,7 +5,7 @@ cd ~/rom
 export CCACHE_DIR=~/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
-ccache -M 20G -F 0
+ccache -M 50G -F 0
 export BUILD_USERNAME="darknius"
 export BUILD_HOSTNAME="darx-labs"
 $lunch
@@ -13,6 +13,6 @@ export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
 export BUILD_BROKEN_DUP_RULES=true
 $make -j8 &
-sleep 97m
+sleep 99m
 kill %1
 ccache -s
