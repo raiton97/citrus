d8e03201f80b20fd50a54c3b79104542e71495f6
raiton97 <133027621+raiton97@users.noreply.github.com>
2023-06-02 20:25:39 +0700

    Add files via upload

--- upload.sh
+++ upload.sh
@@ -1,4 +1,5 @@
-curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/juice/ && ls *.zip)"
-rclone copy ~/rom/out/target/product/chime/Nusantara_v*.zip fm293:chime -P
-curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/juice/ && ls *.zip) Uploaded Successfully!"
-
+if [ -f ~/rom/out/target/product/chime/Nusantara*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/chime/ && ls *.zip)"
+      rclone copy ~/rom/out/target/product/chime/Nusantara_v*.zip fm293:chime -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/chime/ && ls *.zip) Uploaded Successfully!"
+fi
