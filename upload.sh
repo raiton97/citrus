5861d03a529a2a46b6018ac6fea8ee956c4abc6f
raiton97 raikiri <raitonraikiri97@gmail.com>
2023-06-18 09:29:42 +0000

    Try ccache

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
 curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/juice/ && ls *.zip)"
-rclone copy ~/rom/out/target/product/juice/Nusantara_v*.zip 298:juice -P
+rclone copy ~/rom/out/target/product/juice/xdCAF_*.zip 298:juice -P
 curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/juice/ && ls *.zip) Uploaded Successfully!"
 
