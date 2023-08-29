492ba6f413947d277d7ff6c97009a696807c9f8c
raiton97 raikiri <raitonraikiri97@gmail.com>
2023-08-29 08:15:37 +0000

    RRQ CITRUS

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
 curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/citrus/ && ls *.zip)"
-rclone copy ~/rom/out/target/product/citrus/risingOS-v*.zip 298:citrus -P
+rclone copy ~/rom/out/target/product/citrus/RR-*.zip 298:citrus -P
 curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/citrus/ && ls *.zip) Uploaded Successfully!"
 
